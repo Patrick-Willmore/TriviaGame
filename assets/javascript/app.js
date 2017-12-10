a39c579ea0984ea208a18da3fde85086a9525d68
Patrick Willmore <patman1127@gmail.com>
2017-12-10 13:52:41 -0700

    submitFunction added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,108 @@
-
-var userGuess = [0,0,0,0,0,0,0,0,0,0];
-var correctGuesses;
+var userGuess = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var correctAnswers;
+var correctGuesses = 0;
 var wrongGuess;
+// var timer;
+// var minutes;
+// var seconds;
+// var intervalID = setInterval(timeOut(), 10000);
+// var timeRemain = 200000;
 
+// var timerId = setTimeout(function () {
 
+//     }, 1000)
+// timer function
+function startTimer(duration, display) {
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+    // else if (input[type='submit')
+    // }
+  }, 1000);
+}
+
+window.onload = function() {
+  var tenMinutes = 60 * 10,
+    display = document.querySelector("#time");
+  startTimer(tenMinutes, display);
+};
 //====================================================
-$("input[type='submit']").click(function(event){
-    userGuess[0] = $('input[name ="optradio1"]:checked').val()
-    userGuess[1] = $('input[name ="optradio2"]:checked').val()
-    checker();
+// function initializeGame() {
+// intervalID;
+// clearInterval(intervalId);
+//   	clearTimeout(timerID);
+//     timeRemain = setTimeout(timerID(), 1000000);
+
+//     console.log(timerID);
+// }
+
+$("input[type='submit']").click(function(event) {
+  userGuess[0] = $('input[name ="optradio1"]:checked').val();
+  userGuess[1] = $('input[name ="optradio2"]:checked').val();
+  userGuess[2] = $('input[name ="optradio3"]:checked').val();
+  userGuess[3] = $('input[name ="optradio4"]:checked').val();
+  userGuess[4] = $('input[name ="optradio5"]:checked').val();
+//   userGuess[5] = $('input[name ="optradio2"]:checked').val();
+  checker();
+  submit();
+  // myStopFunction();
 });
 
 function checker() {
-    if(userGuess[0] === "4") {
-        console.log("correct");
-        userGuess
-        correctGuesses
+  if (userGuess[0] === "4") {
+    console.log("correct");
+    userGuess[0].push;
+    console.log.userGuess;
+    correctGuesses++;
+  } else if (userGuess === "0") {
+    console.log("incomplete");
+  } else {
+    console.log("incorrect");
+    if (userGuess[1] === "7") {
+      console.log("correct");
+      userGuess[1].push;
+      console.log.userGuess;
+      correctGuesses++;
     } else if (userGuess === "0") {
-        console.log("incomplete");
+      console.log("incomplete");
     } else {
-        console.log("incorrect");
+      console.log("incorrect");
     }
+    if (userGuess[2] === "12") {
+      console.log("correct");
+      userGuess[2].push;
+      console.log.userGuess;
+      correctGuesses++;
+    } else if (userGuess === "0") {
+      console.log("incomplete");
+    } else {
+      console.log("incorrect");
+    }
+  }
+}
+// function myStopFunction() {
+//   clearInterval(minutes);
+//   clearInterval(seconds);
+//   clearTimeout(timer);
+// }
+// function timeOut() {
+//   alert("time is up!");
+// }
+
+function submit(){
+    $("#main").html("<div id = 'newdiv' class = 'jumbotron'>Submitted</div>");
+
+    $('p').html("<p></p>");
 }
